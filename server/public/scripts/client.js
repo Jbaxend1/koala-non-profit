1754e0be913acafb351dc5b03fdea9fd10920a7b
Jonathon Baxendell <j.bax626@gmail.com>
2022-08-04 20:34:34 -0500

    click handler

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -10,54 +10,54 @@ $( document ).ready( function(){
 }); // end doc ready
 
 function setupClickListeners() {
-  $( '#addButton' ).on( 'click', function(){
-    console.log( 'in addButton on click' );
-    // get user input and put in an object
-    // NOT WORKING YET :(
-    // using a test object
-    let koalaToSend = {
-      name: 'testName',
-      age: 'testName',
-      gender: 'testName',
-      ready: 'testName',
-      notes: 'testName',
-    };
-    // call saveKoala with the new obejct
-    saveKoala( koalaToSend );
-  }); 
+  $( '#addButton' ).on( 'click', sendKoalaToServer);
+  
+  // function(){
+  //   console.log( 'in addButton on click' );
+  //   // get user input and put in an object
+  //   // NOT WORKING YET :(
+  //   // using a test object
+  //   let koalaToSend = {
+  //     name: $('#nameIn').val(),
+  //     age: $('#ageIn').val(),
+  //     gender: $('#genderIn').val(),
+  //     ready: $('#readyForTransferIn').val(),
+  //     notes: $('#notesIn').val()
+
+  //   };
+  //   // call saveKoala with the new obejct
+  //   saveKoala( koalaToSend );
+  // }); 
 }
 
 function getKoalas(){
   console.log( 'in getKoalas' );
-  // ajax call to server to get koalas
-  $.ajax ({
+  $.ajax({
     type: 'GET',
-    url: '/koalas',
+    url: '/koalas'
   }).then(function (response) {
-    $('#veiwKoalas').empty();
-    for (let i=0; i<response.length; i++){
-      let koala = response[i];
-        $('#veiwKoalas').append(`
-          <tr>
-            <td>${koala.name}</td>
-            <td>${koala.age}</td>
-            <td>${koala.gender}</td>
-            <td>${koala.ready}</td>
-            <td>${koala.notes}</td>
-          </tr>
-        `)
+    $('#viewKoalas').empty();
+    for (let koala of response) {
+      $('#viewKoalas').append(`
+      <tr>
+        <td>${koala.name}</td>
+        <td>${koala.gender}</td>
+        <td>${koala.age}</td>
+        <td>${koala.ready}</td>
+        <td>${koala.notes}</td>
+      </tr>
+      `)
     }
-  }).catch(function(error){
+  }).catch (function (error){
     console.log('error');
-    alert('something is wrong in GET req');
+    alert('Something Is Wrong');
   })
+  
 } // end getKoalas
 
 function saveKoala( newKoala ){
   console.log( 'in saveKoala', newKoala );
-  // ajax call to server to get koalas
- 
-}
+} // end saveKoala
 
 function sendKoalaToServer () {
   console.log('in sendKoalaToServer');
--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,4 +1,3 @@
-const { Router } = require('express');
 const express = require('express');
 const koalaRouter = express.Router();
 
@@ -55,14 +54,16 @@ const currentInventory = [
 ];
 
 // GET
-
+koalaRouter.get('/', (req, res) => {
+    res.send(currentInventory);
+});
 
 // POST
 koalaRouter.post('/', (req, res) => {
     const koala = req.body;
     currentInventory.push(koala);
-    res.sendStatus(201);
-})
+    res.send(koala);
+  });
 
 // PUT
 
